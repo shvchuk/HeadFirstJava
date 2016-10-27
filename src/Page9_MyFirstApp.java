7ebc8817cfdaa083bda6e4d1203d80ad1dfd1823
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-10-27 14:30:35 +0200

    Initial commit

---
+++ src/Page9_MyFirstApp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Andrzej on 2016-10-27.
+ */
+public class Page9_MyFirstApp {
+
+    public static void main(String[] args) {
+        System.out.println("I rule");
+        System.out.println("The World");
+    }
+}
