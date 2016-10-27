584d4fa12bd841a9f320c38aa7ddf4daee924a8f
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2016-10-27 14:41:43 +0200

    page 12 while loop

---
+++ src/Page12_Loopy.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Andrzej on 2016-10-27.
+ */
+public class Page12_Loopy {
+
+    public static void main(String[] args) {
+
+        int x = 1;
+        System.out.println("Before the loop.");
+
+        while (x < 4) {
+            System.out.println("In the loop.");
+            System.out.println("Value of x is: " + x);
+            x = x + 1;
+        }
+
+        System.out.println("This is after the loop.");
+    }
+}
